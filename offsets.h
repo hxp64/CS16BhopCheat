47a90518db52cba59d498292ac0a9e27bdab1e24
hxp64 <deprodkall@gmail.com>
2026-05-18 22:40:54 -0300

    Add support for GoldClient
    Add support for GoldClient, version 20:10:31 May  7 2025 (10422)

--- offsets.h
+++ offsets.h
@@ -21,7 +21,8 @@ const std::vector<BuildConfig> BUILDS = {
     {"4554", "hl.exe", {"hw.dll", 0x11FDAB4}, {"client.dll", 0x12D048}, {"hw.dll", 0x326800}, {"hw.dll", 0x684210}},
     {"8684", "hl.exe", {"hw.dll", 0x122E2D4}, {"client.dll", 0x131434}, {"hw.dll", 0x6C3AB0}, {"hw.dll", 0x64429C}},
     {"GSClient", "cs.exe", {"engine.dll", 0x122DF54}, {"client.dll", 0x131424}, {"engine.dll", 0x6C3730}, {"engine.dll", 0x643F1C}},
-    {"NextClient", "cs.exe", {"hw.dll", 0x122E2D4}, {"client.dll", 0x131434}, {"hw.dll", 0x6C3AB0}, {"hw.dll", 0x64429C}}
+    {"NextClient", "cs.exe", {"hw.dll", 0x122E2D4}, {"client.dll", 0x131434}, {"hw.dll", 0x6C3AB0}, {"hw.dll", 0x64429C}},
+    {"GoldClient", "hl.exe", {"hw.dll", 0x2F49620}, {"client.dll", 0x1A0EA8}, {"hw.dll", 0x32E6DE8}, {"hw.dll", 0x278FFFC}} // New since May 18, 2026
 };
 
 void RunLogic(const BuildConfig& cfg);
